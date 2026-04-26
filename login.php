157a25b5d9edffd2bb9cd1b7659876d7c24e6597
Sabeeha <sabeehas.24cse@kongu.edu>
2026-04-27 01:37:07 +0530

    fix login null error

--- login.php
+++ login.php
@@ -1,11 +1,19 @@
 <?php
 include "db.php";
 
+// Read JSON input
 $data = json_decode(file_get_contents("php://input"));
+
+// If no input
+if (!$data || !isset($data->cardNo) || !isset($data->pin)) {
+    echo json_encode(["status" => "no_input"]);
+    exit();
+}
 
 $cardNo = $data->cardNo;
 $pin = $data->pin;
 
+// Query DB
 $sql = "SELECT * FROM users WHERE cardNo='$cardNo' AND pin='$pin'";
 $result = $conn->query($sql);
 
