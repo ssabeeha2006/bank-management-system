c365f18ec0374a732d53c7c79ce4843d7cdce19e
Sabeeha <sabeehas.24cse@kongu.edu>
2026-04-27 01:30:30 +0530

    final db fix using env

--- db.php
+++ db.php
@@ -2,19 +2,15 @@
 
 $DATABASE_URL = getenv("DATABASE_URL");
 
-if (!$DATABASE_URL) {
-    die("DATABASE_URL not set");
-}
-
 $db = parse_url($DATABASE_URL);
 
-$host = $db["host"];
-$user = $db["user"];
-$password = $db["pass"];
-$dbname = ltrim($db["path"], "/");
-$port = $db["port"];
-
-$conn = new mysqli($host, $user, $password, $dbname, $port);
+$conn = new mysqli(
+    $db["host"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/"),
+    $db["port"]
+);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
