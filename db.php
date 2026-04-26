294a75042cdf3645202d37ffb43810878660e324
Sabeeha <sabeehas.24cse@kongu.edu>
2026-04-27 01:02:52 +0530

    added correct db password

--- db.php
+++ db.php
@@ -1,14 +1,20 @@
-include "db.php";
 <?php
-$host = "shuttle.proxy.rlwy.net";
-$user = "root";
-$password = "YOUR_PASSWORD"; // paste from Railway
-$database = "railway";
-$port = 54756;
 
-$conn = new mysqli($host, $user, $password, $database, $port);
+$url = "mysql://root:sabeeha@shuttle.proxy.rlwy.net:54756/railway";
+
+$dbparts = parse_url($url);
+
+$host = $dbparts['host'];
+$user = $dbparts['user'];
+$password = $dbparts['pass'];
+$dbname = ltrim($dbparts['path'],'/');
+$port = $dbparts['port'];
+
+$conn = new mysqli($host, $user, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+echo "Connected successfully";
 ?>
